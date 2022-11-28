e5785514c11e7e119f7fae506f976f0fb04376ed
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-11-28 14:39:33 +0600

    my products page is yet to finished

--- src/Pages/Dashboard/Sellers/MyProducts.js
+++ src/Pages/Dashboard/Sellers/MyProducts.js
@@ -1,12 +1,49 @@
+import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../context/AuthProvider';
 
 const MyProducts = () => {
     const { user } = useContext(AuthContext);
+
+    const url = `http://localhost:5000/dashboard/addedproducts?email=${user?.email}`
+
+    const { data: addedproducts = [] } = useQuery({
+        queryKey: ['addedproducts', user?.email],
+        queryFn: async () => {
+            const res = await fetch(url);
+            const data = await res.json();
+            return data;
+        }
+    })
     return (
-        <div>
-            <h2>My products</h2>
+        <div className=' bg-[#E3FFE6] h-full'>
+            <div className="overflow-x-auto">
+                <table className="table w-full text-center">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Selling Price</th>
+                            <th>Location</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            addedproducts &&
+                            addedproducts?.map((bikes, i) =>
+                                <tr className='hover' key={i}>
+                                    <th>{i + 1}</th>
+                                    <td>{bikes.data.bikeName}</td>
+                                    <td>{bikes.data.sellingPrice}</td>
+                                    <td>{bikes.data.location}</td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
+
     );
 };
 
